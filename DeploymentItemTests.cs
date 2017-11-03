6a15061a3a115952d797fe7e1f66e94b78292126
Mertsch <Mertsch@users.noreply.github.com>
2017-11-03 12:10:43 +0100

    Initial commit

---
+++ DeploymentItemTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DeploymentItemTestBug
+{
+    [TestClass]
+    public class DeploymentItemTests
+    {
+        [TestMethod]
+        [DeploymentItem("DeploymentItemTestBug.dll")]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DeploymentItemTestBug")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DeploymentItemTestBug")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("03231b18-95df-40f4-afdc-502c4bdd16d3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
